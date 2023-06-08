26ff5649aed40a86c7d140dcfd7fdefd94ed7d35
falcon_codes <prathamesh.chavanpsc2018@gmail.com>
2023-06-08 23:09:34 +0530

    refactor: Adjusted spacing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ import logo from "@/img/logo.png"
 import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter,faInstagram,faDiscord,faMedium,faTelegram,faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faAmazon } from "@fortawesome/free-brands-svg-icons";
+import cloud1 from "@/img/cloud-1.png"
 
 const socials = [
   {
@@ -48,7 +48,7 @@ const socials = [
 
 const NavItem = ({link, children}) => {
   return <div className="w-12 h-12 rounded-full bg-[#48414d] flex items-center justify-center">
-    <a href={link} className="">{children}</a>
+    <a href={link} >{children}</a>
   </div>
 }
 export default function Home() {
@@ -58,7 +58,7 @@ export default function Home() {
 	return (
 		<main className="text-primary">
 			<header className="z-50 md:block">
-				<div className="px-4 lg:px-40 py-2 text-xs flex items-center justify-between bg-dark text-white">
+				<div className="px-4 lg:px-40 py-2 text-xs flex items-center justify-center md:justify-between bg-dark text-white">
           <Image src={logo} alt="LOGO" className="h-20 w-20"></Image>
 					<div className="hidden md:flex gap-4">
             <NavItem link="https://instagram.com">
@@ -83,13 +83,13 @@ export default function Home() {
 				</div>
 				<div className="hidden lg:flex px-40 py-4 items-center justify-center bg-white text-black">
 					<nav className="font-extrabold space-x-20">
-						<Link href="">HOME</Link>
+						<Link href="#">HOME</Link>
 						<Link href="#about">ABOUT</Link>
-						<Link href="">NFTS</Link>
+						<Link href="#xoxo">NFTS</Link>
 						<Link href="#tokenomics">TOKENOMICS</Link>
 						<Link href="#roadmap">ROADMAP</Link>
 						<Link href="#socials">SOCIALS</Link>
-						<Link href="#xoxo">XOXO</Link>
+						<Link href="#xoxo">PARTNERS</Link>
 					</nav>
 				</div>
 			</header>
@@ -98,21 +98,21 @@ export default function Home() {
 				<div className="flex flex-col justify-center gap-20">
 					<h1
 						data-aos="zoom-in"
-						className="mt-16 text-white text-6xl md:text-7xl lg:text-9xl font-extrabold "
+						className="xl:mt-32 text-white text-6xl md:text-7xl lg:text-8xl 2xl:text-9xl font-extrabold "
 					>
 						JOIN HAPPY CATS
 					</h1>
-					<div className="font-semibold text-white text-lg lg:text-4xl px-4 md:px-[30%]">
+					<div className="font-semibold text-white text-lg lg:text-2xl 2xl:text-4xl px-4 md:px-[20%]">
 						Inspired by the “Happy Cat” viral meme, our own{" "}
 						{"<come up with a fancy name>"} (the Cat) has arrived. The days of
 						Dogs, Peeps are over. It’s time for our “Happy Happy Cat” to take
 						over the Web3 world.
 					</div>
 					<div className="flex justify-around">
-						<button className="px-6 md:px-10 py-4 border border-white bg-white text-lg md:text-2xl text-black font-bold rounded-full baseline">
+						<button className="px-6 py-4 md:px-24 md:py-8 border border-white bg-white text-lg md:text-2xl text-black font-bold rounded-full baseline">
 							<a href="#">Buy Now</a>
 						</button>
-						<button className="px-6 md:px-10 py-4 border border-white bg-white text-lg md:text-2xl text-black font-bold rounded-full baseline">
+						<button className="px-6 py-4 md:px-24 md:py-8 border border-white bg-white text-lg md:text-2xl text-black font-bold rounded-full baseline">
 							<a href="#">Claim Airdrop</a>
 						</button>
 					</div>
@@ -120,6 +120,7 @@ export default function Home() {
 			</SectionContainer>
 
 			<SectionContainer id="about" className="py-40 bg-about">
+        <Image ></Image>
 				<div
 					data-aos="fade"
 					className="flex flex-col items-center justify-center gap-12"
