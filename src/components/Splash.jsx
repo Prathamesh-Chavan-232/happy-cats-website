68e05db8992aa271ae4fd26673e549fd529a2305
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-06-21 01:14:05 +0530

    feat: Loader

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -9,12 +9,14 @@ export const Splash = () => {
 		<SectionContainer className="h-screen w-screen bg-splash bg-cover bg-no-repeat">
 			<div className="flex flex-col items-center justify-center text-center gap-24">
 				<Image src={logo} alt="Logo" className="absolute left-10 h-96 w-96" />
-				<ClimbingBoxLoader
-					speedMultiplier="0.8"
-					color="#fff"
-					size={"25px"}
-					className=""
-				/>
+				<div className="pt-40">
+					<ClimbingBoxLoader
+						speedMultiplier="0.8"
+						color="#fff"
+						size={"25px"}
+						className=""
+					/>
+				</div>
 			</div>
 		</SectionContainer>
 	);
