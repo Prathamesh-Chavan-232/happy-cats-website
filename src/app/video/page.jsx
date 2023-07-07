03923dabc7ca9d1aaf08a3328b1a732095f5c327
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-07-07 22:22:32 +0530

    feat: Back button

--- src/app/video/page.jsx
+++ src/app/video/page.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
 import video from "@/img/happycats.mp4";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 export default function Video() {
+	const router = useRouter();
 	return (
 		<div className="bg-black w-screen h-screen flex items-center justify-center">
 			<button
-				// onClick={useHistory()}
+				onClick={() => {
+					router.back();
+				}}
 				className="bg-white text-black w-12 h-12 rounded-full text-2xl font-extrabold absolute top-[5%] left-[10%]"
 			>
 				<FontAwesomeIcon icon={faArrowLeft} size="xl" />
