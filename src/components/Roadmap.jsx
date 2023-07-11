f0b73b981069b17f355d342648beecf06613b141
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-07-11 20:48:49 +0530

    feat: hamburger menu

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -49,34 +49,36 @@ export const Roadmap = () => {
 			id="roadmap"
 			className="relative bg-roadmap bg-cover bg-no-repeat py-40 w-full flex flex-col gap-12"
 		>
-			<div className="flex flex-col gap-20">
+			<div className="flex flex-col gap-12">
 				<Title className="text-center">ROADMAP</Title>
-				<Image
-					src={bamboo}
-					alt="bamboo"
-					className="hidden lg:block absolute top-60 left-[5%] xl:left-[44%]"
-				></Image>
-				<Image
-					src={bamboo2}
-					alt="bamboo"
-					className="lg:hidden absolute top-60 left-0"
-				></Image>
-				{roadmap.map((item, i) => {
-					return (
-						<div key={i} className={"timeline-elem " + item.align}>
-							<Paragraph className="whitespace-pre-line">
-								<div className="text-4xl">{item.title}</div>
-								<p className="text-xl">{item.content}</p>
-							</Paragraph>
-						</div>
-					);
-				})}
-			</div>
-			<p className="px-4 md:px-[30%] text-center text-lg lg:text-2xl text-black font-semibold">
-				** We may modify/add new items in Roadmap so as to keep up with the
-				latest trends in Blockchain and Crypto Industry. We would always like to
-				bring the best for our community.
-			</p>
-		</div>
-	);
+        <div className="py-12">
+          <Image
+            src={bamboo}
+            alt="bamboo"
+            className="hidden lg:block absolute top-60 top-[10%] left-[44%]"
+          ></Image>
+          <Image
+            src={bamboo2}
+            alt="bamboo"
+            className="lg:hidden absolute top-60 left-0"
+          ></Image>
+          {roadmap.map((item, i) => {
+            return (
+              <div key={i} className={"timeline-elem " + item.align}>
+                <Paragraph className="whitespace-pre-line">
+                  <div className="text-4xl">{item.title}</div>
+                  <p className="text-xl">{item.content}</p>
+                </Paragraph>
+              </div>
+            );
+          })}
+        </div>
+        <p className="px-4 md:px-[30%] text-center text-lg lg:text-2xl text-black font-semibold">
+          ** We may modify/add new items in Roadmap so as to keep up with the
+          latest trends in Blockchain and Crypto Industry. We would always like to
+          bring the best for our community.
+        </p>
+      </div>
+    </div>
+    );
 };
